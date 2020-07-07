d2d456ed17cd9d591dddb16a70ab549d17e7b479
howmun <howmun@gmail.com>
2020-07-07 16:14:07 +0800

    Update index.js
    3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello 2!');
+  res.send('Hello 3!');
 });
 
 var server = app.listen(3000, function () {
