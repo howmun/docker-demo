6381726b81ef3bb36c74b085317cbea1a58f5cca
unknown <howmun.lee@trealtechnology.com>
2020-07-08 12:55:52 +0800

    555

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello 444!');
+  res.send('Hello 555!');
 });
 
 var server = app.listen(3000, function () {
