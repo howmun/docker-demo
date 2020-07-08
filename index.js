b324d09d480ac4a5a29315656b33a0a2e1138daf
unknown <howmun.lee@trealtechnology.com>
2020-07-08 11:43:52 +0800

    4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello 3!');
+  res.send('Hello 444!');
 });
 
 var server = app.listen(3000, function () {
